d876cd126761a1e84be25abe6b4654df81ed2e6c
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-08-21 18:30:23 +0100

    Code: added img attributes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,27 +1,40 @@
 import { useGlobalContext } from "../context";
 
+const Modal = () => {
+  const { closeModal, selectedMeal } = useGlobalContext();
 
-const Modal = ()=>{
+  const {
+    strMealThumb: image,
+    strMeal: title,
+    strInstructions: text,
+    strSource: source,
+  } = selectedMeal;
 
-    const {closeModal, selectedMeal} = useGlobalContext()
-
-    const { strMealThumb: image, strMeal: title, strInstructions: text, 
-            strSource: source } = selectedMeal
-
-    return <aside className="modal-overlay">
-        <div className="modal-container">
-            <img src={image} className="img modal-img alt=" />
-            <div className="modal-content">
-                <h4>{title}</h4>
-                <p> <b>Cooking Instruction</b> </p>
-                <p>{text}</p>
-                <a href={source} target="_blank"> Original Source</a>
-                <button className="btn btn-hipster close-btn" onClick={()=> closeModal()}>Close</button>
-            </div>
-            
+  return (
+    <aside className="modal-overlay">
+      <div className="modal-container">
+        <img src={image} className="img modal-img" alt="meal-image" />
+        <div className="modal-content">
+          <h4>{title}</h4>
+          <p>
+            {" "}
+            <b>Cooking Instruction</b>{" "}
+          </p>
+          <p>{text}</p>
+          <a href={source} target="_blank" rel="noreferrer">
+            {" "}
+            Original Source
+          </a>
+          <button
+            className="btn btn-hipster close-btn"
+            onClick={() => closeModal()}
+          >
+            Close
+          </button>
         </div>
-        
+      </div>
     </aside>
-}
+  );
+};
 
-export default Modal
+export default Modal;
